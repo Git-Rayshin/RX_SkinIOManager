9e2974ce6ce25da9556852f9d0e9e2e7e53f0b76
Git-Rayshin <git.rayshin@gmail.com>
2024-07-05 05:14:01 +0900

    delete last line of pymel

--- skin_io_manager/utils/__init__.py
+++ skin_io_manager/utils/__init__.py
@@ -1,11 +1,10 @@
 import os
 import sys
 import traceback
-import pymel.core as pm
 from PySide2 import QtWidgets, QtCore, QtGui
 from maya import OpenMayaUI as omui
 from shiboken2 import wrapInstance, getCppPointer
-
+from maya import cmds
 _LOGICAL_DPI_KEY = "_LOGICAL_DPI"
 PY2 = sys.version_info[0] == 2
 
@@ -57,9 +56,9 @@ def showDialog(dialog, dInst=True, dockable=False, *args):
     # ensure clean workspace name
     if hasattr(windw, "toolName") and dockable:
         control = windw.toolName + "WorkspaceControl"
-        if pm.workspaceControl(control, q=True, exists=True):
-            pm.workspaceControl(control, e=True, close=True)
-            pm.deleteUI(control, control=True)
+        if cmds.workspaceControl(control, q=True, exists=True):
+            cmds.workspaceControl(control, e=True, close=True)
+            cmds.deleteUI(control, control=True)
     desktop = QtWidgets.QApplication.desktop()
     screen = desktop.screen()
     screen_center = screen.rect().center()
